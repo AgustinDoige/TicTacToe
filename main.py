ab0881ad9283b0995089fc290f608b9b14590e7e
ADoige Desktop <laozoka@gmail.com>
2019-01-04 14:23:28 -0300

    Working version

--- main.py
+++ main.py
@@ -1,42 +1,114 @@
+"""
+0 generally represents a unfinished game
+1 generally represents the Human Player.
+2 generally represents the Bot Player
+3 generally represents a Draw
+"""
 class Board:
-	def __init__(self,window,startingPlayer,startingShape,placements=[[0,0,0],[0,0,0],[0,0,0]]):
-		# Player: 1 or 2. 1 means the Human player, 2 means the Bot
-		# Placements: List of lists. Matrix 3x3.
-		# An element inside placement can be either: 0 Meaning EMPTY, 1 meaning an item placed by the player 1, or 2 meaning an item placed by player 2
-		drawBackground(window)
-		self.player = startingPlayer  #Means the player whos move is NEXT.
-		self.shape = startingShape
+	# placements is a list of lists that holds the matrix of the board. The matrix goes through the columns down instead of rows right
+	def __init__(self,placements,shape,player):
 		self.placements = placements
-		self.window = window
+		self.currentShape = shape    # 'cross' OR 'circle'
+		self.currentPlayer = player  # 1 OR 2 - It means the player who's turn is now.
+		self.state = self.getState()
 
-	def isOccupied(self,i1,i2):
+	def cloneBoard(self):
+		return Board(deepcopy(self.placements),deepcopy(self.currentShape),deepcopy(self.currentPlayer))
+
+	def getState(self):
+		# 	return 0 == UNFINISHED GAME // return 1 == HUMAN WINS  //  return 2 == BOT WINS // return 3 == DRAW
+		plc = self.placements
+		setList = []
+		#First it checks the Diagonals.
+		setList.append(set((plc[0][0],plc[1][1],plc[2][2]))) #Diagonal from top left to down right
+		setList.append(set((plc[0][2],plc[1][1],plc[2][0]))) #Diagonal from down left to up right
+		#Then it checks the columns
+		for i in range(0,3):
+			setList.append(set(plc[i]))
+		#Then it chekcs the rows
+		for i in range(0,3):
+			setList.append(set((plc[0][i],plc[1][i],plc[2][i])))
+		#This checks the setList
+		# print(setList)
+		for st in setList:
+			if len(st) == 1:
+				if 0 not in st:
+					return list(st)[0]
+		#Now that has made sure neither 1 nor 2 have won, it needs to see if the board is a Draw (3) or Unfinished (0)
+		for l in plc:
+			for it in l:
+				if it == 0:
+					return 0
+		return 3
+
+	def value(self):
+		# print("Called value about",self.placements)
+		# Gives the value of the current Board. If the board is not finished, it will make every play making a tree with
+		# the best choices for each player and then it will gather the value of this board
+		# Degenerate case
+		s = self.getState()
+		if s != 0:
+			return s # 1 == HUMAN WINS // 2 == BOT WINS // 3 == DRAW
+
+		optionSet = set()
+		for in1 in range(0,3):
+			for in2 in range(0,3):
+				if not self.occupied(in1,in2):
+					subVal = self.makeMove(in1,in2).value() #Value of making the move (in1,in2)
+					if subVal == self.currentPlayer:
+						# If there's a move that leads to victory, the value of that board is propagated to this one
+						# because we can predict that the player will make that move
+						return subVal
+					optionSet.add(subVal) #It adds other values that aren't victories
+		if 3 in optionSet:
+			# If it can't find a victory, the optimal solution is a Draw, so we propagate that result
+			return 3
+		else:
+			# It gets to this option when all the options of moves can't lead to a victory for CurrentPlayer, nor a Draw
+			return self.opponent()
+
+	def occupied(self,i1,i2):
 		# From (0,0) to (2,2)
 		if self.placements[i1][i2] == 0:
 			return False
 		return True
 
-	def nextTurn(self,i1,i2):
+	def opponent(self):
+		# Returns 2 when self.currentPlayer == 1 and viceversa
+		if self.currentPlayer == 1:
+			return 2
+		return 1
+
+	def makeMove(self,i1,i2):
+		# Returns a CLONE of board with move (i1,i2) made
 		# i1 and i2 have to range from 0 to 2
-		self.placements[i1][i2] = self.player
-		drawShape(window=self.window,position=(i1+1,i2+1),shape=self.shape)
-
-		if self.shape == 'cross':
-			self.shape = 'circle'
+		clone = self.cloneBoard()
+		clone.placements[i1][i2] = self.currentPlayer
+		if clone.currentShape == 'circle':
+			clone.currentShape = 'cross'
 		else:
-			self.shape = 'cross'
-		
-		if self.player == 1:
-			self.player = 2
-		else:
-			self.player = 1
+			clone.currentShape = 'circle'
 
-	def drawIt(self):
+		if clone.currentPlayer == 1:
+			clone.currentPlayer = 2
+		else:
+			clone.currentPlayer = 1
+		clone.state = clone.getState()
+		return clone
+
+	def drawMove(self,i1,i2,window):
+		# Draws move on window and returns a CLONE of board with move (i1,i2) made
+		# i1 and i2 have to range from 0 to 2
+		drawShape(window=window,position=(i1,i2),shape=self.currentShape)
+		# print("Drawing a",self.currentShape,"in",i1,i2,)
+		return self.makeMove(i1,i2)
+
+	def drawEntireBoard(self,window):
 		tDic = {1:'cross',2:'circle'}
 		i = 0
 		for l in self.placements:
 			j = 0
 			for it in l:
-
 				if it != 0:
 					drawShape(self.window,position=(i+1,j+1),shape=tDic[it])
 				j+=1
@@ -77,65 +149,6 @@ class Button:
 		self.rectangle.undraw()
 		self.text.undraw()
 
-def hasEnded(placements):
-	"""
-	return 0 == UNFINISHED GAME
-	return 1 == HUMAN WINS
-	return 2 == BOT WINS
-	return 3 == DRAW
-	return 4 == BOTH WIN
-
-	placements coord are (x,y) with origin on the top left going larger "right-down"
-	So a value on the top right is a [[0,0,0],[0,0,0],[1,0,0]] (x=2,y=0)
-	"""
-
-	hW = False
-	bW = False
-	# First it checks the diagonals
-	d1 = set((placements[0][0],placements[1][1],placements[2][2]))
-	d2 = set((placements[0][2],placements[1][1],placements[2][0]))
-	for d in [d1,d2]:
-		if len(d) == 1:
-			if 0 not in d:
-				if 1 in d:
-					hW = True
-				else:
-					bW = True
-
-	# Then checks the columns
-	for l in placements:
-		r = set(l)
-		if len(r) == 1:
-			if 0 not in r:
-				if 1 in r:
-					hW = True
-				else:
-					bW = True
-
-	# Then it checks the rows
-	for i in range(0,3):
-		c = set((placements[i][0],placements[i][1],placements[i][2]))
-		if len(c) == 1:
-			if 0 not in c:
-				if 1 in c:
-					hW = True
-				else:
-					bW = True
-
-	# Finally checks for a draw
-	for l in placements:
-		for item in l:
-			if item == 0:
-				return 0
-
-	if hW and bW:
-		return 4
-	if hW:
-		return 1
-	if bW:
-		return 2
-	return 3
-
 def isPointInRectangle(point,rectangle):
 	# Takes objects Point and checks if their coordenades are in rectangle. Rectangle is NOT an object, but a tuple with 2 tuples that each have the
 	# x and y coord of the extreme points of the rectangle, like the way Graphics.py constructs rectangles
@@ -152,113 +165,22 @@ def isPointInRectangle(point,rectangle):
 		return False
 	return True
 
-def boardValue(placements,turn=2):
-	# Returns the value of a board for the bot to chose
-	# turn = 1 means it's the players turn, turn = 2 means it's the  bot's turn
-	state = hasEnded(placements) # 1 = Human Wins, 2 = Bot wins, 3 = Draw, 0 = Non Finished
-	if state != 0:
-		if state == 1:
-			return (-1,True)
-			# global humanWins
-			# humanWins += 1
-		if state == 2:
-			# global botWins
-			# botWins  += 1
-			return (1,True)
-		elif state == 3:
-			# global draws
-			# draws += 1
-			return (0,True)
-		else:
-			print("Error! with boardValue()",placements,turn)
-
-	if turn == 2: #BotTurn
-		mx = -1
-		for in1 in range(0,3):
-			for in2 in range(0,3):
-				if placements[in1][in2] == 0:
-					plCopy = deepcopy(placements)
-					plCopy[in1][in2] = 2
-					val = boardValue(plCopy,1)[0]
-					if val > mx:
-						mx = deepcopy(val)
-						if mx == 1:
-							return (mx,False)
-
-		# print(placements,mx)	
-		return (mx,False)
-
-	elif turn == 1: #HumanTurn
-		mn = 1
-		for in1 in range(0,3):
-			for in2 in range(0,3):
-				if placements[in1][in2] == 0:
-					plCopy = deepcopy(placements)
-					plCopy[in1][in2] = 1
-					val = boardValue(plCopy,1)[0]
-					if val < mn:
-						mn = deepcopy(val)
-						if mn == -1:
-							return (mn,False)
-
-		# print(placements,mn)
-		return (mn,False)
-
-	else:
-		print("Problem:",placements,turn)
-
-def botMove(board):
-	plc = board.placements
-
-	mx = -1
-	moveDic = {}
-	for in1 in range(0,3):
-		for in2 in range(0,3):
-			if plc[in1][in2] == 0:
-				cpyplc = deepcopy(plc)
-				cpyplc[in1][in2] = 2
-				boardVal = boardValue(cpyplc,1)
-				if boardVal[1]:
-					board.nextTurn(in1,in2)
-				if boardVal[0] > mx:
-					mx = boardVal[0]
-				moveDic[(in1,in2)] = boardVal[0]
-	options = []
-	for key in moveDic.keys():
-		if moveDic[key] == mx:
-			options.append(key)
-	print(mx,moveDic,options,board.placements)
-	chosenMove = choice(options)
-	board.nextTurn(chosenMove[0],chosenMove[1])
-
-def playerMove(window,board):
-	# Takes a board when it is its turn and changes it with the player move
-	done = False
-	while not done:
-		clickPoint = window.getMouse()
-		for i in range(0,3):
-			for j in range(0,3):
-				rect = ((xSide*i,ySide*j),(xSide*(i+1),ySide*(j+1)))
-				if isPointInRectangle(clickPoint,rect):
-					if not board.isOccupied(i,j):
-						board.nextTurn(i,j)
-						done = True
-
 def drawShape(window,position,shape):
 	# Position is a 2 item tuple with the position of one of the 9 spots on the bord. 
-	# (2,2) is the center. (1,1) is the top left spot. (1,2) is the middle spot on the top row
+	# (1,1) is the center. (0,0) is the top left spot. (2,0) is the top right spot
 	# shape can be 'cross' or 'circle'
 
 	smallRatio = 9
 
-	xPoint = int(xSide/2) + (xSide * (position[0]-1))
-	yPoint = int(ySide/2) + (ySide * (position[1]-1))
+	xPoint = int(xSide/2) + (xSide * (position[0]))
+	yPoint = int(ySide/2) + (ySide * (position[1]))
 
 	if shape == 'circle':
 		c = Circle(Point(xPoint,yPoint),int(min(window.width,window.height)/smallRatio))
 		c.setOutline(color_rgb(200,30,30))
 		c.setWidth(6)
 		c.draw(window)
+		drawnShapes.append(c)
 
 	elif shape == 'cross':
 		d = int(min(window.width,window.height)/smallRatio)
@@ -270,11 +192,48 @@ def drawShape(window,position,shape):
 		line2.setWidth(6)
 		line1.draw(window)
 		line2.draw(window)
+		drawnShapes.extend([line1,line2])
 
 	else:
 		while True:
 			print("Shape '{}' Not Known".format(shape))
 			sleep(60)
+
+def playerMove(window,board):
+	# Takes a board when it is its turn and changes it with the player move
+	# print(board,type(board))
+	while True:
+		clickPoint = window.getMouse()
+		for i in range(0,3):
+			for j in range(0,3):
+				rect = ((xSide*i,ySide*j),(xSide*(i+1),ySide*(j+1)))
+				if isPointInRectangle(clickPoint,rect):
+					if not board.occupied(i,j):
+						return board.drawMove(i,j,window)
+
+def botMove(window,board):
+	# sleep(0.5)
+	optionsDictionary = {} 
+	for in1 in range(0,3):
+		for in2 in range(0,3):
+			if not board.occupied(in1,in2):
+				optionsDictionary[(in1,in2)] = board.makeMove(in1,in2).value()
+	bestOptions = []
+	if 2 in optionsDictionary.values():
+		for key in optionsDictionary.keys():
+			if optionsDictionary[key] == 2:
+				bestOptions.append(key)
+
+	elif 3 in optionsDictionary.values():
+		for key in optionsDictionary.keys():
+			if optionsDictionary[key] == 3:
+				bestOptions.append(key)
+
+	else:
+		bestOptions = deepcopy(list(optionsDictionary.keys()))
+
+	chosenBox = choice(bestOptions)
+	return board.drawMove(chosenBox[0],chosenBox[1],window) 
 
 def drawBackground(window):
 	window.setBackground(color_rgb(15,15,15))
@@ -290,31 +249,7 @@ def drawBackground(window):
 		line2.setOutline(color_rgb(230,230,230))
 		line2.setWidth(4)
 		line2.draw(window)
-
-def endGame(window,status):
-	if status == 1:
-		txt = "Player Wins!"
-	elif status == 2:
-		txt = "You lose."
-	else:
-		txt = "It's a Draw"
-	line = Line(Point(15,int(h/2)),Point(w-15,int(h/2)))
-	line.setOutline(color_rgb(220,220,0))
-	line.setWidth(50)
-	line.draw(window)
-	text = Text(Point(int(w/2),int(h/2)),txt) #The anchor point is on the CENTER of the text
-	text.setTextColor(color_rgb(0,0,0))
-	text.setSize(20)
-	text.setFace('helvetica')
-	text.draw(window)
-
-def whoStarts(window):
-	# Returns the variable startingPlayer
-	pass
-
-def playerShape(window,startingPlayer):
-	# Returns the variable startingShape
-	pass
+		drawnShapes.extend([line1,line2])
 
 def launchScreen(window):
 	window.setBackground(color_rgb(10,10,70))
@@ -331,8 +266,6 @@ def launchScreen(window):
 
 	startButton = Button(window,"START",(25,37),25)
 	startButton.draw()
-	# (self,window,text,relativeCoord,size):
-#########################
 	while True:
 		click = window.getMouse()
 		if startButton.wasClicked(click):
@@ -341,74 +274,166 @@ def launchScreen(window):
 			text2.undraw()
 			break
 
-def testA():
-	# print(boardValue([[1,0,1],[0,1,2],[2,0,2]],1)[0])
-	# print(boardValue([[1,1,1],[0,1,2],[2,0,2]],2)[0])
-	# print(boardValue([[0,0,2],[0,1,0],[0,0,0]],1)[0])
-	# print(boardValue([[2,0,0],[0,1,0],[0,0,0]],1)[0])
+def winTitle(window,status):
+	if status == 1:
+		txt = "Player Wins!"
+	elif status == 2:
+		txt = "You lose."
+	else:
+		txt = "It's a Draw"
+	line = Line(Point(15,int(h/2)),Point(w-15,int(h/2)))
+	line.setOutline(color_rgb(220,220,0))
+	line.setWidth(50)
+	line.draw(window)
+	text = Text(Point(int(w/2),int(h/2)),txt) #The anchor point is on the CENTER of the text
+	text.setTextColor(color_rgb(0,0,0))
+	text.setSize(20)
+	text.setFace('helvetica')
+	text.draw(window)
+	drawnShapes.extend([text,line])
 
+def pickShape(window):
+	window.setBackground(color_rgb(15,15,10))
+	text = Text(Point(int(w/2),int(h/5)),"Pick your shape")
+	text.setTextColor(color_rgb(230,230,80))
+	text.setSize(25)
+	text.setFace('helvetica')
+	text.draw(window)
 
-	global botWins
-	global humanWins
-	global draws 
-	botWins = 0
-	humanWins = 0
-	draws = 0
-	boardValue([[0,0,0],[0,0,0],[0,0,0]],1)
-	print("botWins:",botWins,". humanWins:",humanWins,". draws",draws)
-	humanWins = 0
-	botWins = 0
-	draws = 0
-	boardValue([[0,0,0],[0,0,0],[0,0,0]],2)
-	print("botWins:",botWins,". humanWins:",humanWins,". draws",draws)
+	crossButton = Button(window,"X",(35,40),25)
+	circleButton  = Button(window,"O",(15,40),25)
+	crossButton.draw()
+	circleButton.draw()
 
-def testB():
-	import itertools
-	a = (0,1,2)
-	gen = itertools.product(a,repeat=9)
-	botW = 0
-	humW = 0
-	draw = 0
-	both = 0
-	incl = 0
-	for it in gen:
-		l = list((list(it[0:3]),list(it[3:6]),list(it[6:])))
+	c = Circle(Point(w*15/50,h*25/50),int(min(w,h)/7))
+	c.setOutline(color_rgb(200,30,30))
+	c.setWidth(7)
+	c.draw(window)
 
-		st = hasEnded(l)
-		if st != 0:
-			if st == 1:
-				humW += 1
-				print(st,l)
-			elif st == 2:
-				botW += 1
-			elif st == 3:
-				draw += 1
-			else:
-				both += 1
-		else:
-			incl += 1
-	print("botWins:",botW,". humanWins:",humW,". draws:",draw,". both:",both,". incomplete:",incl)
-	print(botW+humW+draw+both+incl)
+	d = int(min(w,h)/7)
+	line1 = Line(Point(w*35/50-d,h*25/50-d),Point(w*35/50+d,h*25/50+d))
+	line2 = Line(Point(w*35/50-d,h*25/50+d),Point(w*35/50+d,h*25/50-d))
+	line1.setOutline(color_rgb(30,30,200))
+	line2.setOutline(color_rgb(30,30,200))
+	line1.setWidth(7)
+	line2.setWidth(7)
+	line1.draw(window)
+	line2.draw(window)
 
-	win = GraphWin("TATETI",501,501)
-	global h
-	h = win.height
-	global w
-	w = win.width
-	global xSide
-	xSide = int(w/3)
-	global ySide
-	ySide = int(h/3)
-	plac = [[1, 1, 2], [1, 2, 2], [2, 2, 1]]
-	# window,startingPlayer,startingShape,placements=[[0,0,0],[0,0,0],[0,0,0]]
-	brd = Board(win,1,'cross',plac)
-	brd.drawIt()
-	win.getMouse()
-	win.close()
+	while True:
+		d = False
+		click = window.getMouse()
+		if crossButton.wasClicked(click):
+			sh = 'cross'
+			d = True
+		if circleButton.wasClicked(click):
+			sh = 'circle'
+			d = True
+
+		if d:
+			crossButton.undraw()
+			circleButton.undraw()
+			text.undraw()
+			line1.undraw()
+			line2.undraw()
+			c.undraw()
+			return sh
+
+def whoStarts(window):
+	window.setBackground(color_rgb(50,50,70))
+	text = Text(Point(int(w/2),int(h/5)),"Do you want to go first?")
+	text.setTextColor(color_rgb(230,230,80))
+	text.setSize(25)
+	text.setFace('helvetica')
+	text.draw(window)
+
+	yesButton = Button(window,"YES",(15,37),25)
+	noButton  = Button(window,"NO ",(36,37),25)
+	yesButton.draw()
+	noButton.draw()
+	while True:
+		d = False
+		click = window.getMouse()
+		if yesButton.wasClicked(click):
+			d = True
+			starter = 1
+		if noButton.wasClicked(click):
+			d = True
+			starter = 2
+		if d:
+			text.undraw()
+			noButton.undraw()
+			yesButton.undraw()
+			return starter
+
+def cleanWindow(window):
+	for a in range(0,len(drawnShapes)):
+		drawnShapes.pop(0).undraw()
+
+def backLoop(window):
+	window.getMouse()
+	cleanWindow(window)
+	window.setBackground(color_rgb(5,5,40))
+	text = Text(Point(int(w/2),int(h/5)),"")
+	text.setTextColor(color_rgb(230,230,80))
+	text.setSize(25)
+	text.setFace('helvetica')
+	text.draw(window)
+
+	menuButton = Button(window,"MENU",(25,15),25)
+	rematchButton  = Button(window,"REMATCH ",(25,25),25)
+	endButton = Button(window,"CLOSE",(25,35),25)
+	menuButton.draw()
+	rematchButton.draw()
+	endButton.draw()
+	while True:
+		d = False
+		click = window.getMouse()
+		if menuButton.wasClicked(click):
+			d = True
+			action = 'restart'
+		if rematchButton.wasClicked(click):
+			d = True
+			action = 'rematch'
+		if endButton.wasClicked(click):
+			d = True
+			action = 'end'
+		if d:
+			text.undraw()
+			menuButton.undraw()
+			rematchButton.undraw()
+			endButton.undraw()
+			return action
+
+def menu(win):
+	launchScreen(win)
+	global shapE
+	shapE = pickShape(win)
+	global startEr
+	startEr = whoStarts(win)
+	game(win)
+
+def game(win):
+	drawBackground(win)
+	b = Board([[0,0,0],[0,0,0],[0,0,0]],shapE,startEr)
+	if startEr == 1:
+		while b.state == 0:
+			b = playerMove(win,b)
+			if b.state != 0:
+				break
+			b = botMove(win,b)
+		winTitle(win,b.state)
+	elif startEr == 2:
+		while b.state == 0:
+			b = botMove(win,b)
+			if b.state != 0:
+				break
+			b = playerMove(win,b)
+		winTitle(win,b.state)
+	# print(drawnShapes)
 
 def main():
 	win = GraphWin("TATETI",501,501)
-
 	global h
 	h = win.height
 	global w
@@ -417,38 +442,22 @@ def main():
 	xSide = int(w/3)
 	global ySide
 	ySide = int(h/3)
+	global drawnShapes
+	drawnShapes = []
 
-	launchScreen(win)
-
-	b = Board(window=win,startingPlayer=1,startingShape='cross')
+	menu(win)
 	while True:
-		playerMove(win,b)
-		status = hasEnded(b.placements)
-		if status > 0:
-			endGame(win,status)
+		des = backLoop(win)
+		if des == 'rematch':
+			game(win)
+		elif des == 'restart':
+			menu(win)
+		else:
+			win.close()
 			break
-		botMove(b)
-		status = hasEnded(b.placements)
-		if status > 0:
-			endGame(win,status)
-			break
-	win.getMouse()
-	win.close()
 
 from random import choice
 from time import sleep
 from copy import deepcopy
-from copy import copy
 from graphics import *
 main()
-# testA()
-# testB()
-
-
-###
-"""
-RESULTS:
-- Checking all posible board combinations:
-botWins: 178 . humanWins: 178 . draws: 114 . both: 42 . incomplete: 19171		  TOTAL; 19683
-"""
-###
